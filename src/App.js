68bd1cdedda566c527dd3ac22df9100565d1d132
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-10-12 17:49:19 +0800

    dynamic links for topics created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import ErrorPage from './components/error-page/ErrorPage';
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
+import QuizPage from './components/QuizPage/QuizPage';
 import RootPage from './components/root-page/RootPage';
 
 
@@ -17,7 +18,21 @@ function App() {
           path: '/',
           loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <HomePage></HomePage>,
-        }
+        },
+        {
+          path: '/topics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <HomePage></HomePage>,
+        },
+        {
+
+          element: <QuizPage />,
+          path: "/quiz/:id",
+          loader: async ({ params }) => {
+            return fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`);
+          }
+        },
+
       ],
       errorElement: <ErrorPage></ErrorPage>,
     }
---
+++ src/components/QuizPage/QuizPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuizPage = () => {
+    return (
+        <div>
+            <h2>Quiz Page</h2>
+        </div>
+    );
+};
+
+export default QuizPage;

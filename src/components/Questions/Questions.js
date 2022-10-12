1e9cafc9f50af445846e05d392435dab088b1128
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-10-12 19:29:29 +0800

    right wrong answer functionality added

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -17,10 +17,27 @@ const Questions = ({ questionDetail }) => {
         )
     }
 
+    const checkAns = (op) => {
+        if (op === correctAnswer) {
+            Swal.fire({
+                icon: 'success',
+                title: 'WoW... You chose wisely.',
+                showConfirmButton: true,
+            })
+        }
+        else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...not the right answer',
+                text: 'Try again!',
+            })
+        }
+    }
+
     return (
         <div className='shadow-lg w-5/6 md:w-3/5 lg:w-2/5 mx-auto my-12 p-4 rounded-lg'>
             <h2 className='text-purple-600'>Question: {question} <span onClick={showAns}>{element}</span></h2>
-            <p>{options.map(op => <p className='border-2 border-emerald-900 p-4 m-4 rounded-lg hover:bg-orange-200'> {op}</p>)}</p>
+            <p>{options.map(op => <p className='border-2 border-emerald-900 p-4 m-4 rounded-lg hover:bg-orange-200' onClick={() => checkAns(op)}> {op}</p>)}</p>
         </div>
     );
 };
